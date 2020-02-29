aa0a3851fcd217d28160f93838d5dec5e22432f2
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-02-28 17:11:16 -0800

    Added console text to output on webpage for start of game

--- RPSscript.js
+++ RPSscript.js
@@ -1,4 +1,12 @@
 
+let playerPoints = _score("playerScores").innerHTML = 0
+let computerPoints = _score("computerScores").innerHTML = 0
+
+function _score(playerScores, computerScores)
+{
+  return document.getElementById(playerScores, computerScores);
+}
+
 function computerPlay()
 {
     //computer randomly returns either 'Rock', 'Paper', or 'Scissors'
@@ -8,13 +16,23 @@ function computerPlay()
     return randomChoice; //i need to use return because I want this value to be used.
 }
 
+/***
+ * This function lets us get the player's input, and start the round
+ ***/
+function startRound(event){
+  let playerSelection = event.target.closest("button").value;
+    computerSelection = computerPlay();
+  console.log(playerSelection);
+    let result = playRound(playerSelection, computerSelection);
+}
+
 
 function playRound(playerSelection, computerSelection)
 {
-    //debug line console.log("Playing the round. Computer: "+computerSelection+", Player: "+playerSelection);
 
     if (playerSelection == 'rock' && computerSelection == 'rock')
     {   
+        //can i use textContent or appendChild() here to update the console on screen?
         console.log("Tie!");
     }
     else if (playerSelection == 'rock' && computerSelection == 'paper')
@@ -62,9 +80,11 @@ function playRound(playerSelection, computerSelection)
     
 } 
 
-function game()
+
+/*function game()
 {
   let computerPoints = 0, playerPoints = 0;
+  
 
   for (let i = 0; i < 5; i++)
   {
@@ -104,6 +124,32 @@ function game()
       console.log("You both have the same number of points! It's a tie!");
     }
   
+} my old game() function above. Commenting out for now may need to pull some info */
+
+//All player choice buttons are disabled upon web page load
+document.getElementById("rockDisabled").disabled = true;
+document.getElementById("paperDisabled").disabled = true;
+document.getElementById("scissorsDisabled").disabled = true;
+
+function game()
+{
+  let consoleClear = document.querySelector('.gameScore');
+  let consoleText = '<i>Go!</i>';
+  consoleClear.innerHTML = consoleText;
+
+  let computerPoints = 0, playerPoints = 0;
+  const playerOptions = document.querySelectorAll(".player-option");
+  
+  playerOptions.forEach(button =>
+  {
+    button.addEventListener("click", startRound);
+  })
+
+  document.getElementById("rockDisabled").disabled = false;
+  document.getElementById("paperDisabled").disabled = false;
+  document.getElementById("scissorsDisabled").disabled = false;
+  //after clicking start button, buttons are active.
+  
 }
 
 const startButton = document.querySelector('.startButton');
@@ -113,21 +159,27 @@ startButton.addEventListener('click', () =>
   console.log('start button clicked');
 });
 
-const rock = document.querySelector('.rock');
-rock.addEventListener('click', () =>
-{
-  playRound(rock);
-  console.log('rock button clicked');
-});
+  const rock = document.querySelector('.rock');
+  rock.addEventListener('click', () =>
+  {
+    playRound(rock);
+    console.log('rock button clicked');
+  });
 
-const paper = document.querySelector('.paper');
-paper.addEventListener('click', () =>
-{
-  playRound(paper);
-});
+  const paper = document.querySelector('.paper');
+  paper.addEventListener('click', () =>
+  {
+    playRound(paper);
+    console.log('paper button clicked');
+  });
 
-const scissors = document.querySelector('.scissors');
-scissors.addEventListener('click', () =>
-{
-  playRound(scissors);
-});
+  const scissors = document.querySelector('.scissors');
+  scissors.addEventListener('click', () =>
+  {
+    playRound(scissors);
+    console.log('scissors button clicked');
+  });
+
+
+//see discord messages in JS section from 9:30am - 10:37am. Also snow's codepen help:
+// https://codepen.io/snowmonkey/pen/MWwmLmg?editors=0010
